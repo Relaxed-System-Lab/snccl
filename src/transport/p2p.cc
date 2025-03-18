f245b20e914af1d2ef1297da07b53fc2395e3304
王嘉树 <wangjiashu@wangjiashudeMacBook-Pro.local>
2025-03-18 13:09:10 +0800

    test

--- src/transport/p2p.cc
+++ src/transport/p2p.cc
@@ -120,6 +120,8 @@ extern int64_t ncclParamMNNVLEnable();
 
 /* Determine if two peers can communicate through p2p */
 ncclResult_t p2pCanConnect(int* ret, struct ncclComm* comm, struct ncclTopoGraph* graph, struct ncclPeerInfo* info1, struct ncclPeerInfo* info2) {
+  *ret = 1;
+  return ncclSuccess;
   initCeOperation();
 
   // Check topology / p2p level.
