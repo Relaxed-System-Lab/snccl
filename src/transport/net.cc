c0c91d63e51540438cec37383f4c9b7b32d42059
王嘉树 <wangjiashu@wangjiashudeMacBook-Pro.local>
2025-03-18 23:12:40 +0800

    test

--- src/transport/net.cc
+++ src/transport/net.cc
@@ -151,12 +151,12 @@ struct netRegInfo {
 
 /* Determine if two peers can communicate with NET */
 static ncclResult_t canConnect(int* ret, struct ncclComm* comm, struct ncclTopoGraph* graph, struct ncclPeerInfo* info1, struct ncclPeerInfo* info2) {
-  *ret = 1;
-  return ncclSuccess;
+  INFO(NCCL_INIT, "jiashu: %d, %d", info1->hostHash, info1->hostHash)
   if (info1->hostHash == info2->hostHash) {
     // If on the same host, check intra-node net is not disabled.
     NCCLCHECK(ncclTopoCheckNet(comm->topo, info1->rank, info2->rank, ret));
   }
+  *ret = 1;
   return ncclSuccess;
 }
 
