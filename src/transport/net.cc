1ceb04239ec39810b02720bb1d245f4527ea879c
王嘉树 <wangjiashu@wangjiashudeMacBook-Pro.local>
2025-03-22 03:31:27 +0800

    test

--- src/transport/net.cc
+++ src/transport/net.cc
@@ -319,7 +319,7 @@ static ncclResult_t sendConnect(struct ncclComm* comm, struct ncclConnect* conne
   struct connectMap* map = (connectMap*) send->transportResources;
   void* opId;
   int recvUseGdr;
-  INFO(NCCL_INIT, "jiashu connect %d %d", nranks, rank);
+  //INFO(NCCL_INIT, "jiashu connect %d %d", nranks, rank);
   memcpy(&recvUseGdr, (uint8_t*)connectInfo + sizeof(ncclNetHandle_t), sizeof(int));
   if (!recvUseGdr) send->conn.flags &= ~NCCL_DIRECT_NIC;
 
