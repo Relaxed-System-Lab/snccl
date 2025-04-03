4b881d9f35b7584527706449c03c4dbe7442ad24
王嘉树 <wangjiashu@wangjiashudembp.tail76b2d.ts.net>
2025-04-03 12:29:53 +0800

    test

--- src/server.cc
+++ src/server.cc
@@ -26,7 +26,8 @@ static void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
         }
     } else if (ev == MG_EV_READ) {
         // 1. 解析 client1 数据包中的目标地址
-        struct mg_str *data = &c->recv;
+        struct mg_iobuf *recv_buf = &c->recv;
+        struct mg_str data = mg_str_n((const char *)recv_buf->buf, recv_buf->len);
         if (data->len < 4) return; // 等待完整包头
         
         uint16_t addr_len = mg_ntohs(*(uint16_t *)data->buf);
@@ -67,7 +68,8 @@ void* ncclserverInit(void* args){
 static void fn(struct mg_connection *c, int ev, void *ev_data) {
     if (ev == MG_EV_READ) {
         // 1. 解析 server1 转发的数据包
-        struct mg_str *data = &c->recv;
+        struct mg_iobuf *recv_buf = &c->recv;
+        struct mg_str data = mg_str_n((const char *)recv_buf->buf, recv_buf->len);
         uint16_t addr_len = mg_ntohs(*(uint16_t *)data->buf);
         char *target_addr = (char *)data->buf + 2;
         uint16_t payload_len = mg_ntohs(*(uint16_t *)(target_addr + addr_len));
