11277568c605c78c40831b84bc3ed935b82eaf11
王嘉树 <wangjiashu@wangjiashudembp.tail76b2d.ts.net>
2025-04-03 16:09:05 +0800

    test

--- src/misc/socket.cc
+++ src/misc/socket.cc
@@ -798,13 +798,15 @@ ncclResult_t ncclSocketInit(struct ncclSocket* sock, const union ncclSocketAddre
   addr_in.sin_addr.s_addr = inet_addr("192.168.1.148"); // IP 地址（网络字节序）
   memset(addr_in.sin_zero, 0, 8);              // 填充 8 字节空数据
 
-  memset(&sock->addr, 0, sizeof(union ncclSocketAddress));  // 清空结构体
   // 将 sockaddr_in 转换为 sockaddr 的 sa_data 
-  memcpy(sock->addr->sa_data, &addr_in.sin_port, 2);       // 前 2 字节为端口
-  memcpy(sock->addr->sa_data + 2, &addr_in.sin_addr.s_addr, 4); // 中间 4 字节为 IP
-  memset(sock->addr->sa_data + 6, 0, 8);                   // 后 8 字节填充（sin_zero）
-  sock->addr->sa_family = AF_INET;
-  sock->addr->salen = sizeof(struct sockaddr_in);
+  struct sockaddr sa;
+  memcpy(sa.sa_data, &addr_in.sin_port, 2);       // 前 2 字节为端口
+  memcpy(sa.sa_data + 2, &addr_in.sin_addr.s_addr, 4); // 中间 4 字节为 IP
+  memset(sa.sa_data + 6, 0, 8);                   // 后 8 字节填充（sin_zero）
+  sa->sa_family = AF_INET;
+  sa->salen = sizeof(struct sockaddr_in);
+  
+  memset(&sock->addr, sa, sizeof(union ncclSocketAddress));  // 清空结构体
   sock->salen = sizeof(struct sockaddr_in);
   NCCLCHECKGOTO(socketResetFd(sock), ret, fail);
 
