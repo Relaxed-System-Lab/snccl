d2f38255adc86493cc78fdf4d073af4fe2c7651b
王嘉树 <wangjiashu@wangjiashudembp.tail76b2d.ts.net>
2025-04-03 16:42:06 +0800

    test

--- src/misc/socket.cc
+++ src/misc/socket.cc
@@ -809,7 +809,7 @@ ncclResult_t ncclSocketInit(struct ncclSocket* sock, const union ncclSocketAddre
   NCCLCHECKGOTO(socketResetFd(sock), ret, fail);
 
   char line[SOCKET_NAME_MAXLEN+1];
-  INFO(NCCL_INIT, "jiashu: changing %s to %s", ncclSocketToString(&sock->addr, line), ncclSocketToString(&sock->backupAddr, line));
+  INFO(NCCL_INIT|NCCL_NET, "jiashu: changing %s to %s", ncclSocketToString(&sock->addr, line), ncclSocketToString(&sock->backupAddr, line));
 
 exit:
   return ret;
